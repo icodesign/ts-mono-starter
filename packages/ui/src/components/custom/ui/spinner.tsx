fc8c12099fb5e8e88c7d67c6bd1b6e2a344d7c99
icodesign <git@icodesign.me>
2024-07-25 09:12:30 +0800

    Minor ui update

--- packages/ui/src/components/custom/ui/spinner.tsx
+++ packages/ui/src/components/custom/ui/spinner.tsx
@@ -1,9 +1,18 @@
-export function CircleSpinner() {
+import { cn } from "@ui/lib/utils";
+
+type Props = {
+  className?: string;
+};
+
+export function CircleSpinner(props: Props) {
   return (
     <>
       <svg
         aria-hidden="true"
-        className="w-4 h-4 me-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className={cn(
+          "w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600",
+          props.className
+        )}
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"

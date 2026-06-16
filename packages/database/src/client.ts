a0d364441ac5ae9a677ba40523d19e1f1891141c
Lance Wang <git@icodesign.me>
2026-06-16 13:22:17 +0800

    Update postgres driver

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,20 +1,20 @@
-import { drizzle as drizzlePostgres } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { drizzle as drizzlePostgres } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 import * as schema from "./schema";
 
 function createPostgresClient(databaseUrl: string) {
-  return postgres(databaseUrl, {
+  return new Pool({
+    connectionString: databaseUrl,
     max: 5,
-    fetch_types: false,
-    prepare: true,
   });
 }
 
 export function createDatabase(databaseUrl: string) {
   const client = createPostgresClient(databaseUrl);
 
-  return drizzlePostgres(client, {
+  return drizzlePostgres({
+    client,
     schema,
   });
 }
